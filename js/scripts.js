15fd660318e34aef71c170c87d7a39c662572d98
Eugeniu Nicolenco <clipstard@mail.ru>
2019-02-07 09:58:05 +0200

    added lei images on front page table

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,22 @@
+var lei = [1, 5, 10 , 20, 50, 100, 200, 500, 1000];
 var bani = [1, 5, 10, 25, 50];
-var currentBan = 0;
+var currentBan = 9990;
 
-function getNext() {
+var getNext = function() {
     currentBan++;
     if (currentBan >= bani.length) currentBan = 0;
     return bani[currentBan];
-}
+};
 
 function init() {
-    var button1 = button.getItem(1);
-    button1.addEventListener('click', function () {
-        alert(1)
-    });
     setInterval(() => {
         header.setImage(getNext());
     }, 1500);
+
+    for(let i = 0; i < lei.length; i++) {
+        image.insert(image.id('lei'), lei[i], false);
+        image.insert(image.id('lei_verso'), lei[i], true);
+    }
 }
 
 var header = {
@@ -29,13 +31,23 @@ var header = {
     }
 
 };
-var button = {
-    getItem: function (id) {
-        return document.getElementById(this.getFullId(id));
-    },
-    getFullId: function (id) {
-        return 'button' + id.toString();
-    }
+var image = {
+  insert: function (id, value, type = false) {
+    var elem = document.createElement('div');
+    var image = document.createElement('img');
+    image.width = 380;
+    image.height = 180;
+    image.src = (type) ?
+        'img/lei_' + value + '_verso.png' :
+        'img/lei_' + value + '.png';
+    elem.append(image);
+    id.append(elem);
+  },
+
+  id: function (elem) {
+      return document.getElementById(elem);
+  }
+
 };
 
 
