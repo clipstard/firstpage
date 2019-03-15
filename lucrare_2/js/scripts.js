27d269f79ca507de4d0761daf03e41f9520a4c6d
Eugeniu Nicolenco <clipstard@mail.ru>
2019-03-15 19:20:43 +0200

    final commit

--- lucrare_2/js/scripts.js
+++ lucrare_2/js/scripts.js
@@ -1,4 +1,5 @@
-var menuItems = ['home', 'imaginary']
+var menuItems = ['home', 'imaginary'];
+var blocks = [11, 12, 13, 21, 22, 23, 31, 32, 33];
 
 var menuManager = {
     id: function (name) {
@@ -13,9 +14,9 @@ var menuManager = {
         }
         elem.addClass('active');
     },
-    init: function (){
+    init: function () {
         let _this = this;
-        for(let item of menuItems) {
+        for (let item of menuItems) {
             this.elem(item).on('click', function () {
                 _this.activate(_this.elem(item));
             })
@@ -23,15 +24,119 @@ var menuManager = {
         _this.activate(_this.elem(menuItems[0]));
     }
 };
-
+var scrollIndex = 50;
 $(document).ready(function () {
-   menuManager.init();
-   var i = 50;
-   var scrollDetector = 0;
-    $('body').css({backgroundColor: 'rgb(72, 68,' + i++ + ')'})
-   $(document).on('scroll', function () {
-       ($(document).scrollTop() > scrollDetector) ? i++ : i--;
-       scrollDetector = $(document).scrollTop();
-      $('body').css({backgroundColor: 'rgb(72,68,' + i + ')'})
-   });
+    $('#blockFormular').hide('fast');
+    menuManager.init();
+    var scrollDetector = 0;
+    $('body').css({backgroundColor: 'rgb(72, 68,' + scrollIndex + ')'});
+    $(document).on('scroll', function () {
+        ($(document).scrollTop() > scrollDetector) ? scrollIndex++ : scrollIndex--;
+        scrollDetector = $(document).scrollTop();
+        $('body').css({backgroundColor: 'rgb(72,68,' + scrollIndex + ')'});
+        Animation.trigger(scrollIndex);
+    });
+
 });
+
+
+var Animation = {
+    init: function () {
+
+    },
+    trigger: function (i) {
+        Animation.block1(i);
+        Animation.block2(i);
+        Animation.block3(i);
+        Animation.blockFormular();
+        Animation.reset();
+    },
+    block1: function (i) {
+        $('#block11').css({left: (i - 48) * (10 + i / 5)}).css({top: 120});
+        $('#block12').css({left: (i - 63) * (10 + i / 5)}).css({top: 120});
+        $('#block13').css({left: (i - 78) * (10 + i / 5)}).css({top: 120});
+    },
+    block2: function (i) {
+        $('#block21').css({left: (i - 93) * (10 + i / 5)}).css({top: 400});
+        $('#block22').css({left: (i - 108) * (10 + i / 5)}).css({top: 400});
+        $('#block23').css({left: (i - 123) * (10 + i / 5)}).css({top: 400});
+    },
+    block3: function (i) {
+        $('#block31').css({left: (i - 138) * (10 + i / 5)}).css({top: 720});
+        $('#block32').css({left: (i - 153) * (10 + i / 5)}).css({top: 720});
+        $('#block33').css({left: (i - 168) * (10 + i / 5)}).css({top: 720});
+    },
+    blockFormular: function () {
+        if ($(document).scrollTop() > 5000) {
+            $('#blockFormular').fadeIn('slow');
+            Animation.hideOthers();
+        } else {
+            $('#blockFormular').fadeOut('fast');
+            Animation.showOthers();
+        }
+    },
+    reset: function () {
+        if ($(document).scrollTop() === 0) scrollIndex = 50;
+    },
+    status: false,
+    hideOthers: function () {
+        for (let i of blocks) {
+            $('#block' + i).hide('fast');
+        }
+    },
+    showOthers: function () {
+        for (let i of blocks) {
+            $('#block' + i).show('fast');
+        }
+    }
+
+};
+
+var Basic = {
+  id: function(index) {
+      return $('#' + index);
+  }
+};
+var Form = {
+    _this: this,
+    errors: [],
+    validate: function() {
+        this.errors = [];
+       this
+           .checkLength('materialRegisterFormFirstName')
+           .checkLength('materialRegisterFormLastName')
+           .checkLength('materialRegisterFormPassword')
+           .checkEmail('materialRegisterFormEmail')
+        ;
+    },
+    checkLength: function(id) {
+        Basic.id(id).removeClass('has-error');
+        if (Basic.id(id).val().length < 3) {
+            this.errors.push(id);
+        }
+        return this;
+    },
+    checkEmail: function(id) {
+        if (Basic.id(id).val().length <= 3) {
+            this.errors.push(id);
+        } else if (!~Basic.id(id).val().indexOf('@') || !~Basic.id(id).val().indexOf('.')) {
+            this.errors.push(id);
+        }
+        return this;
+    },
+    submit: function(){
+        this.validate();
+        if (!this.errors.length) {
+            $('form').submit();
+        }else {
+            for (let error of this.errors) {
+                Basic.id(error).addClass('has-error');
+            }
+        }
+    }
+};
+
+function test() {
+    Form.submit();
+    return false;
+}
