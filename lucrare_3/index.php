4d1f68394d8fc445220ae383b1a97bc41ef2c98e
Eugeniu Nicolenco <clipstard@mail.ru>
2019-03-27 22:15:34 +0200

    added basic files for controller

--- lucrare_3/index.php
+++ lucrare_3/index.php
@@ -1,4 +1,21 @@
 <?php
+$request = $_SERVER['REDIRECT_URL'];
+echo "INDEX";
 
-echo "<h1> Hello World</h1>";
-
+switch ($request) {
+    case '/' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '/about' :
+        require __DIR__ . '/views/about.php';
+        break;
+    case '/test' :
+        require __DIR__ . '/views/test.php';
+        break;
+    default:
+        require __DIR__ . '/views/404.php';
+        break;
+}
---
+++ lucrare_3/views/about.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eugeniu
+ * Date: 3/27/19
+ * Time: 9:19 PM
+ */
+
+var_dump($_SERVER);
